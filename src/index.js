cad417bf60ecda281afae85ea163e9f9333cbf05
Sami Barghshoon <sdbar@Samis-MacBook-Pro.local>
2018-11-22 15:06:52 -0500

    added higher order component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import App from 'components/App';
 import Root from 'root';
+import './styles/app.css'; 
 
 ReactDOM.render(
-  <Root><App /></Root>,
+  <Root>
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
+  </Root>,
   document.getElementById('root')
 );
